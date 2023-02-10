256d377c89cc2b4d5e0005350b2612720f1ab0c8
perepeljuk-09 <77107021+perepeljuk-09@users.noreply.github.com>
2023-02-10 20:32:54 +0300

    added animation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Lottie from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Lottie source={require('./assets/animation.json')} autoPlay loop />
       <StatusBar style="auto" />
     </View>
   );
